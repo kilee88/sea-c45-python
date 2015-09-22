4dc7ab714405340a6f9341c6cdf2ee454ad581ac
Ki <leeki@u.washington.edu>
2015-09-21 19:48:50 -0700

    hw07 completed

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -15,4 +15,16 @@ def lucas(n):
     elif n > 1:
         return (lucas(n - 2) + lucas(n - 1))
 
-def sum_series(x,y,z):
+
+def sum_series(n, y=0, z=1):
+    if y == 0 and z == 1:
+        return fibonacci(n)
+    elif y == 2 and z == 1:
+        return lucas(n)
+    else:
+        if n == 0:
+            return y
+        elif n == 1:
+            return z
+        else:
+            return (sum_series(n - 2, y, z) + sum_series(n - 1, y, z))
