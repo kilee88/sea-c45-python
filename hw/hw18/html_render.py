5c69b0144b88fedf2be7acac35f3da2852d6f6d3
Ki <leeki@u.washington.edu>
2015-10-02 10:39:25 -0700

    hw18

--- hw/hw18/html_render.py
+++ hw/hw18/html_render.py
@@ -8,4 +8,14 @@ Python class example.
 # The start of it all:
 # Fill it all in here.
 class Element(object):
-    pass
+    def __init__(self, name=""):
+        self.name = name
+        self.children = []
+
+    def append(self, child):
+        self.child = children.append(child)
+
+    def render(self, f):
+        f.write("<{name}>".format(name=self.name))
+        # TODO: WRITE OUT THE CHILDREN HERE
+        f.write("<{name}>".format(name=self.name))
