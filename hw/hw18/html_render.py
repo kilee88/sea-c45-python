0a5a10e5d0f2084041f4357f895b53a56ce33e73
Ki <leeki@u.washinf>
2015-10-07 19:22:21 -0700

    added first part

--- hw/hw18/html_render.py
+++ hw/hw18/html_render.py
@@ -7,15 +7,36 @@ Python class example.
 
 # The start of it all:
 # Fill it all in here.
+
+
 class Element(object):
+
     def __init__(self, name=""):
         self.name = name
         self.children = []
 
     def append(self, child):
-        self.child = children.append(child)
+        self.children.append(child)
 
     def render(self, f):
-        f.write("<{name}>".format(name=self.name))
-        # TODO: WRITE OUT THE CHILDREN HERE
-        f.write("<{name}>".format(name=self.name))
+        f.write("\n<{name}>".format(name=self.name))
+        for child in self.children:
+            child.render(f)
+        f.write("\n</{name}>".format(name=self.name))
+
+
+class Html(Element):
+    def __init__(self):
+        super(Html, self).__init__(name="html")
+
+
+class Body(Element):
+    def __init__(self):
+        super(Body, self).__init__(name="body")
+
+
+class P(Element):
+    def __init__(self, content):
+        super(P, self).__init__(name="p")
+        #self.content = content
+        self.append(content)
